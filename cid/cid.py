9f776266a88c72ca1256433011535b53a353a6e3
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2017-09-08 23:41:28 +0400

    refactor the API and CID classes and add tests

--- cid/cid.py
+++ cid/cid.py
@@ -3,12 +3,12 @@ import base58
 
 import multibase
 
-from morphys import ensure_bytes
+from morphys import ensure_bytes, ensure_unicode
 
 import multicodec
 
 
-class CID(object):
+class BaseCID(object):
     __hash__ = object.__hash__
 
     def __init__(self, version, codec, multihash):
@@ -28,66 +28,128 @@ class CID(object):
     def multihash(self):
         return self._multihash
 
-    @classmethod
-    def is_cid(self, cidstr):
-        pass
-
-    @classmethod
-    def from_string(cls, cidstr):
-        cidbytes = ensure_bytes(cidstr, 'utf-8')
-        return cls.from_bytes(cidbytes)
-
-    @classmethod
-    def from_bytes(cls, cidbytes):
-        if multibase.is_encoded(cidbytes):
-            cid = multibase.decode(cidbytes)
-            data = cid[1:]
-            version = int(cid[0])
-            codec = multicodec.get_codec(data)
-            multihash = multicodec.remove_prefix(data)
-
-            return cls(version, codec, multihash)
-        else:
-            raise ValueError('input is not multibase encoded')
-
-    @classmethod
-    def from_multihash(cls, multihash):
-        return cls(0, 'dag-pb', multihash)
-
-    @classmethod
-    def from_b58_multihash(cls, b58_multihash):
-        multihash = base58.b58decode(b58_multihash)
-        return cls.from_multihash(multihash)
-
-    @classmethod
-    def from_cid(cls, cid):
-        if multibase.is_encoded(cid):
-            cid = multibase.decode(cid)
-
-    def _buffer(self):
-        if self.version == 0:
-            return self.multihash
-        elif self.version == 1:
-            return b''.join([self.version, multicodec.add_prefix(self.codec, self.multihash)])
-
-    def to_base_encoded_string(self, base='base58btc'):
-        return multibase.encode(base, self._buffer())
-
-    def __str__(self):
-        if self._version == 0:
-            return '{}{}{}'.format(self._version, self._codec, self._multihash.decode('utf-8'))
-        elif self._version == 1:
-            return
-
     def __repr__(self):
-        truncate = lambda x, l: (x[:l] + '..' if len(x) > l else x)
+        def truncate(s, length):
+            return s[:length] + b'..' if len(s) > length else s
 
+        truncate_length = 20
         return '{class_}(version={version}, codec={codec}, multihash={multihash})'.format(
             class_=self.__class__.__name__,
             version=self._version,
             codec=self._codec,
-            multihash=truncate(self._multihash, 50),
+            multihash=truncate(self._multihash, truncate_length),
         )
+
+    def __str__(self):
+        ensure_unicode(self.buffer)
 
     def __eq__(self, other):
         return (self.version == other.version) and (self.codec == other.codec) and (self.multihash == other.multihash)
+
+
+class CIDv0(BaseCID):
+    CODEC = 'dag-pb'
+
+    def __init__(self, multihash):
+        super(CIDv0, self).__init__(0, self.CODEC, multihash)
+
+    @property
+    def buffer(self):
+        return self.multihash
+
+    def encode(self):
+        return base58.b58encode(self.buffer)
+
+    def to_v1(self):
+        return CIDv1(self.CODEC, self.multihash)
+
+
+class CIDv1(BaseCID):
+    def __init__(self, codec, multihash):
+        super(CIDv1, self).__init__(1, codec, multihash)
+
+    @property
+    def buffer(self):
+        return b''.join([bytes([self.version]), multicodec.add_prefix(self.codec, self.multihash)])
+
+    def encode(self, encoding='base58btc'):
+        return multibase.encode(encoding, self.buffer)
+
+    def to_v0(self):
+        if self.codec != CIDv0.CODEC:
+            raise ValueError('CIDv1 can only be converted for codec {}'.format(CIDv0.CODEC))
+
+        return CIDv0(self.multihash)
+
+
+def make_cid(*args):
+    """
+
+    :param args: can be multiple types
+        make_cid(<base58 encoded multihash CID>) -> CIDv0
+        make_cid(<multihash CID>) -> CIDv0
+        make_cid(<multibase encoded multihash CID>) -> CIDv1
+        make_cid(<version>, <codec>, <multihash>) -> CIDv1
+    :return: CIDv0 or CIDv1
+    """
+    if len(args) == 1:
+        data = args[0]
+        if isinstance(data, str):
+            return from_string(data)
+        elif isinstance(data, bytes):
+            return from_bytes(data)
+        else:
+            raise ValueError('invalid argument passsed, expected: str or byte, found: {}'.format(type(data)))
+
+    elif len(args) == 3:
+        version, codec, multihash = args
+        if version not in (0, 1):
+            raise ValueError('version should be 0 or 1, {} was provided'.format(version))
+        if not multicodec.is_codec(codec):
+            raise ValueError('invalid codec {} provided, please check'.format(codec))
+        if not (isinstance(multihash, str) or isinstance(multihash, bytes)):
+            raise ValueError('invalid type for multihash provided, should be str or bytes')
+
+        if version == 0:
+            if codec != CIDv0.CODEC:
+                raise ValueError('codec for version 0 can only be {}, found: {}'.format(CIDv0.CODEC, codec))
+            return CIDv0(multihash)
+        else:
+            return CIDv1(codec, multihash)
+    else:
+        raise ValueError('invalid number of arguments, expected 1 or 3')
+
+
+def is_cid(cidstr):
+    try:
+        return bool(make_cid(cidstr))
+    except ValueError:
+        return False
+
+
+def from_string(cidstr):
+    cidbytes = ensure_bytes(cidstr, 'utf-8')
+    return from_bytes(cidbytes)
+
+
+def from_bytes(cidbytes):
+    if multibase.is_encoded(cidbytes):
+        # if the bytestream is multibase encoded
+        cid = multibase.decode(cidbytes)
+        data = cid[1:]
+        version = int(cid[0])
+        codec = multicodec.get_codec(data)
+        multihash = multicodec.remove_prefix(data)
+
+        return make_cid(version, codec, multihash)
+    elif cidbytes[0] in (0, 1):
+        # if the bytestream is a CID
+        version = cidbytes[0]
+        data = cidbytes[1:]
+        codec = multicodec.get_codec(data)
+        multihash = multicodec.remove_prefix(data)
+
+        return make_cid(version, codec, multihash)
+    else:
+        # otherwise its just base58-encoded multihash
+        return make_cid(0, CIDv0.CODEC, base58.b58decode(cidbytes))
--- tests/test_cid.py
+++ tests/test_cid.py
@@ -1,25 +1,132 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Tests for `cid` package."""
-
+import multihash
 import pytest
 
-
-from cid import cid
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+import multibase
+import multicodec
+from cid import CIDv0, CIDv1, make_cid, is_cid
+from multibase.multibase import CODECS
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.fixture(scope='session')
+def test_hash():
+    return multihash.digest(b'hello world', 'sha2-256').encode()
+
+
+class CIDv0TestCase(object):
+    @pytest.fixture()
+    def cid(self, test_hash):
+        return CIDv0(test_hash)
+
+    def test_init(self, cid, test_hash):
+        """ #__init__: initializes all the attributes correctly """
+        assert cid.version == 0
+        assert cid.codec == CIDv0.CODEC
+        assert cid.multihash == test_hash
+
+    def test_buffer(self, cid, test_hash):
+        """ .buffer: is computed properly """
+        assert cid.buffer == test_hash
+
+    def test_encode(self, cid):
+        """ #encode: base58 encodes the buffer by default """
+        assert cid.encode() == 'QmaozNR7DZHQK1ZcU9p7QdrshMvXqWK6gpu5rmrkPdT3L4'
+
+
+class CIDv1TestCase(object):
+    TEST_CODEC = 'dag-pb'
+
+    @pytest.fixture()
+    def cid(self, test_hash):
+        return CIDv1(self.TEST_CODEC, test_hash)
+
+    def test_init(self, cid, test_hash):
+        """ #__init__: attributes are set properly """
+        assert cid.version == 1
+        assert cid.codec == self.TEST_CODEC
+        assert cid.multihash == test_hash
+
+    def test_buffer(self, cid):
+        """ .buffer: buffer is computed properly """
+        buffer = cid.buffer
+        assert buffer[0] == 1
+        assert buffer[1:] == multicodec.add_prefix(self.TEST_CODEC, cid.multihash)
+
+    def test_encode_default(self, cid):
+        """ #encode defaults to base58btc encoding """
+        assert cid.encode() == b'zdj7WhuEjrB52m1BisYCtmjH1hSKa7yZ3jEZ9JcXaFRD51wVz'
+
+    @pytest.mark.parametrize('codec', CODECS)
+    def test_encode_encoding(self, cid, codec):
+        """ #encode uses the encoding provided for encoding """
+        assert cid.encode(codec.encoding) == multibase.encode(codec.encoding, cid.buffer)
+
+
+class CIDTestCase(object):
+    def test_cidv0_eq_cidv0(self, test_hash):
+        """ check for equality for CIDv0 for same hash """
+        assert CIDv0(test_hash) == make_cid(CIDv0(test_hash).encode())
+
+    def test_cidv0_neq(self):
+        """ check for inequality for CIDv0 for different hashes """
+        assert CIDv0(b'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n') != \
+            CIDv0(b'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1o')
+
+    def test_cidv0_eq_cidv1(self, test_hash):
+        """ check for equality between converted v0 to v1 """
+        assert CIDv0(test_hash).to_v1() == CIDv1(CIDv0.CODEC, test_hash)
+
+    def test_cidv1_eq_cidv0(self):
+        """ check for equality between converted v1 to v0 """
+        assert CIDv1(CIDv0.CODEC, test_hash).to_v0() == CIDv0(test_hash)
+
+    def test_cidv1_to_cidv0_no_dag_pb(self):
+        """ converting non dag-pb CIDv1 should raise an exception """
+        with pytest.raises(ValueError) as excinfo:
+            CIDv1('base2', test_hash).to_v0()
+        assert 'can only be converted for codec' in str(excinfo.value)
+
+    def test_is_cid_valid(self, test_hash):
+        assert is_cid(CIDv0(test_hash).encode())
+
+    @pytest.mark.parametrize('test_data', (
+        'foobar',
+        b'foobar',
+        multibase.encode('base58btc', b'foobar'),
+    ))
+    def test_is_cid_invalid(self, test_data):
+        assert not is_cid(test_data)
+
+
+class MakeCIDTestCase(object):
+    def test_hash(self):
+        pass
+
+    def test_hash_invalid_type(self):
+        pass
+
+    def test_version_invalid(self):
+        pass
+
+    def test_codec_invalid(self):
+        pass
+
+    def test_multihash_invalid(self):
+        pass
+
+    def test_version_0_invalid_codec(self):
+        pass
+
+
+class FromStringTestCase(object):
+    def test_multibase_encoded_hash(self):
+        pass
+
+    def test_base58_encoded_hash(self):
+        pass
+
+    def test_cid(self):
+        pass
+
+
+class FromBytesTestCase(FromStringTestCase):
+    pass
