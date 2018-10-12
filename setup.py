342d9e912095e6ea1af9ec44abdb429a94f468a4
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2018-10-12 19:26:09 +0400

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='py-cid',
-    version='0.1.5',
+    version='0.1.6',
     description="Self-describing content-addressed identifiers for distributed systems",
     long_description=readme + '\n\n' + history,
     author="Dhruv Baldawa",
