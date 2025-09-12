c76f743942e7d41a4e96cbf33b87d83feef44d6c
pacrob <5199899+pacrob@users.noreply.github.com>
2025-09-12 15:12:50 -0600

    Bump version: 0.3.0 → 0.3.1

--- cid/__init__.py
+++ cid/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Dhruv Baldawa"""
 __email__ = "dhruv@dhruvb.com"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .cid import CIDv0, CIDv1, from_bytes, from_string, is_cid, make_cid  # noqa: F401
